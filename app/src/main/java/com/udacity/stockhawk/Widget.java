491604dda56cf093d08756affed2eb7061140574
Alexander Rogin <gonetc99@gmail.com>
2017-04-04 17:49:16 +0300

    SAVEPOINT

--- app/src/main/java/com/udacity/stockhawk/Widget.java
+++ app/src/main/java/com/udacity/stockhawk/Widget.java
@@ -10,7 +10,7 @@ public class Widget extends AppWidgetProvider {
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
         for (int appWidgetId : appWidgetIds) {
-            RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.collection_widget);
+            RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.widget);
             views.setRemoteAdapter(R.id.widget_list, new Intent(context, StockWidgetService.class));
 
             appWidgetManager.updateAppWidget(appWidgetId, views);
